6bc3798278f099376f339c45caf948da55e1b0d6
Aaron HeRo <aaron.hero1103@gmail.com>
2025-06-11 17:59:47 -0600

    Refactor MainWindow constructor for clarity and organization

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -4,27 +4,38 @@
 #include <QWidget>
 #include <QMessageBox>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent) {
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+
+    /* Paleta de Colores 
+    primary_color = "#4F48EC";
+    enphasis_color = "#FFBF18";
+    dark_bg = "#141414";
+    light_bg = "#FFFFFF";
+    */
 
     // Crear el widget central
     QWidget *central = new QWidget(this);
     setCentralWidget(central);
 
+    // Configurar la ventana principal
+    this->setStyleSheet("background-color: #141414; font-family: Arial;");
+    this->move(100, 100); // Mover la ventana a una posición específica
+    this->resize(400, 600); // Redimensionar la ventana a un tamaño específico
+
     // Crear un botón
-    boton = new QPushButton("Haz clic aquí", this);
+//    boton = new QPushButton("Haz clic aquí", this);
 
     // Layout vertical
     QVBoxLayout *layout = new QVBoxLayout();
-    layout->addWidget(boton);
+//    layout->addWidget(boton);
 
     // Asignar el layout al widget central
     central->setLayout(layout);
 
     // Conectar el clic del botón a una función lambda
-    connect(boton, &QPushButton::clicked, this, []() {
+/*    connect(boton, &QPushButton::clicked, this, []() {
         QMessageBox::information(nullptr, "Mensaje", "¡Hola desde Qt sin Designer!");
-    });
+    });*/
 }
 
 MainWindow::~MainWindow() {
